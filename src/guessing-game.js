65248b6a33b6526c26df65874750f85a9af204b6
Pavel Logunov <paveloguniv@gmail.com>
2023-02-26 15:41:24 +0900

    feat: add a solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        return this.res = Math.round((this.left + this.right)/2);
     }
 
     lower() {
-
+        this.right = this.res;
     }
 
     greater() {
-
+        this.left = this.res;
     }
 }
 
